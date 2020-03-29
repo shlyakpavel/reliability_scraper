0c94b87e3c338f23eb8511a200618bc2f702497b
VlastelinPoro <62465465+VlastelinPoro@users.noreply.github.com>
2020-03-29 15:51:45 +0300

    polishing parser.py
    deleting unnecessary spaces and etc.

--- parser.py
+++ parser.py
@@ -1,49 +1,46 @@
-  
+import re
 from yargy.interpretation import fact
 from yargy import rule, Parser, or_, not_, and_
 from yargy.predicates import eq, type
 from yargy.pipelines import morph_pipeline
-import re
 
 def yargy_parser(path):
-    RULE = fact (
+    RULE = fact(
         'RULE',
-        ['name','tresh','num']
+        ['name', 'tresh', 'num']
     )
-
     INT = type('INT')
     PUNCT = type('PUNCT')
 
-    DOT = or_(eq('.'),eq(','))
+    DOT = or_(eq('.'), eq(','))
 
     NAME_avail = morph_pipeline(
         [
-        'System Reliability',
-        'availability'
+            'System Reliability',
+            'availability'
         ]
     ).interpretation(
         RULE.name
     )
 
     NAME_mtbf = morph_pipeline(
-            [
-                'MTBF',
-                'mean time between',
-                'mean time between failures',
-                'mean time between failure',
-            ]
-        ).interpretation(
+        [
+            'MTBF',
+            'mean time between',
+            'mean time between failures',
+            'mean time between failure',
+        ]
+    ).interpretation(
         RULE.name
     )
 
     NAME_mttr = morph_pipeline(
-            [
-                'MTTR',
-                'mean time to',
-                'Mean Time To Repair',
-                'repair time',
-            ]
-
+        [
+            'MTTR',
+            'mean time to',
+            'Mean Time To Repair',
+            'repair time',
+        ]
     ).interpretation(
         RULE.name
     )
@@ -52,59 +49,62 @@ def yargy_parser(path):
     NUM_avail = or_(rule(INT, DOT, INT))
 
     UNIT_mtbf = morph_pipeline(
-            [
-                '年',
-                'year',
-                'years',
-                'hour',
-                'hours',
-                'год',
-                'час',
-                'h',
-                '小时'
-            ]
-        )
-
-    UNIT_mttr  = morph_pipeline(
-            [
-                'hour',
-                'hours',
-                'час',
-                'h',
-                '小时'
-            ]
-        )
-
-    X_mtbf = rule(NUM_MTBF, UNIT_mtbf.optional()).interpretation(
-        RULE.num
+        [
+            'year',
+            'years',
+            'hour',
+            'hours',
+            'год',
+            'час',
+            'h',
+        ]
     )
 
-    X_mttr = rule(INT, UNIT_mttr.optional()).interpretation(
-        RULE.num
+    UNIT_mttr = morph_pipeline(
+        [
+            'hour',
+            'hours',
+            'час',
+            'h',
+        ]
     )
 
-    X_avail = rule(NUM_avail, PUNCT.optional()).interpretation(
-        RULE.num
-    )
-    TRESH = rule(and_(not_(eq(NUM_MTBF)),or_(not_(eq(NAME_mttr)),not_(eq(NAME_mtbf))),not_(eq(UNIT_mtbf)), not_(eq(DOT)),
-                 not_(eq(INT)) , not_(eq(X_mttr)), not_(eq(X_mtbf)))).interpretation(
-        RULE.tresh
-    )
+    X_mtbf = rule(NUM_MTBF, UNIT_mtbf.optional()
+                 ).interpretation(
+                     RULE.num
+                 )
 
-    rule_1 = (rule(NAME_mtbf ,(TRESH.optional()).repeatable(),  X_mtbf).repeatable()
+    X_mttr = rule(INT, UNIT_mttr.optional()
+                 ).interpretation(
+                     RULE.num
+                 )
+
+    X_avail = rule(NUM_avail, PUNCT.optional()
+                  ).interpretation(
+                      RULE.num
+                  )
+    TRESH = rule(and_(not_(eq(NUM_MTBF)), or_(not_(eq(NAME_mttr)),
+                                              not_(eq(NAME_mtbf))),
+                      not_(eq(UNIT_mtbf)), not_(eq(DOT)),
+                      not_(eq(INT)), not_(eq(X_mttr)), not_(eq(X_mtbf)))
+                ).interpretation(
+                    RULE.tresh
+                )
+
+    rule_1 = (rule(NAME_mtbf, (TRESH.optional()).repeatable(), X_mtbf).repeatable()
              ).interpretation(
-        RULE
-    )
+                 RULE
+             )
 
-    rule_2 = (rule(NAME_mttr ,(TRESH.optional()).repeatable(),  X_mttr).repeatable()
+    rule_2 = (rule(NAME_mttr, (TRESH.optional()).repeatable(), X_mttr).repeatable()
              ).interpretation(
-        RULE
-    )
+                 RULE
+             )
 
-    rule_3 = (rule(NAME_avail,(TRESH.optional()).repeatable(), X_avail).repeatable()
+    rule_3 = (rule(NAME_avail, (TRESH.optional()).repeatable(), X_avail).repeatable()
              ).interpretation(
-        RULE
-    )
+                 RULE
+             )
 
     f = open(path, 'r')
     text = f.read()
@@ -114,7 +114,8 @@ def yargy_parser(path):
 
     #Temporary workaround. Fix it to site by site processing later
     n = 500
-    text = [line[i-5 if i-5>0 else 0:i+n+5 if i+n+5 < len(line) else len(line) -1] for i in range(0, len(line), n)]
+    text = [line[i-5 if i-5 > 0 else 0:i+n+5 if i+n+5 < len(line)
+                 else len(line) -1] for i in range(0, len(line), n)]
     MEASURE = rule(or_(X_avail, NAME_avail, X_mttr, X_mtbf, NAME_mttr, NAME_mtbf))
     new_line = []
     #Parser #1 text preprocessing
@@ -122,9 +123,9 @@ def yargy_parser(path):
     for line in text:
         matches = list(parser.findall(line))
         spans = [_.span for _ in matches]
-        new_span = [0,0]
-        if len(spans)>=2:
-            for i in range(0,len(spans)-1,1):
+        new_span = [0, 0]
+        if len(spans) >= 2:
+            for i in range(0, len(spans)-1, 1):
                 mini = 1000000
                 maxi = 0
                 if spans[i][0] < mini:
@@ -133,13 +134,13 @@ def yargy_parser(path):
                 if spans[i+1][1] > maxi:
                     new_span[1] = spans[i+1][1]
                     maxi = spans[i+1][1]
-                for i in range(new_span[0],new_span[1]):
+                for i in range(new_span[0], new_span[1]):
                     new_line.append(line[i])
                 new_line.append(' \n ')
     new_line = ''.join(new_line)
     new_line = new_line.split('\n')
     LIST = []
-    MEASURE = or_(rule_1,rule_2, rule_3).interpretation(
+    MEASURE = or_(rule_1, rule_2, rule_3).interpretation(
         RULE
     )
     #Parser #2 Parsing reliability metrics.
@@ -153,20 +154,16 @@ def yargy_parser(path):
                     LIST.append(match.fact)
     return LIST
 
+
 def finding_num(b):
-    names_mtbf = ['mtbf',
-                'mean time between',
-                'mean time between failures',
-                'mean time between failure',]
-    names_mttr = ['mttr',
-                'mean time to',
-                'mean time to repair',
-                'mean time to repairs',
-                 'repair time']
-    names_avail = ['system reliability',
-                  'availability']
-    dict_num = {'MTTR':{},'MTBF':{}, 'System Reliability':{}}
-    dict_max = {'MTTR':0,'MTBF':0,'System Reliability':0}
+    names_mtbf = ['mtbf', 'mean time between',
+                  'mean time between failures',
+                  'mean time between failure',]
+    names_mttr = ['mttr', 'mean time to', 'mean time to repair',
+                  'mean time to repairs', 'repair time']
+    names_avail = ['system reliability', 'availability']
+    dict_num = {'MTTR':{}, 'MTBF':{}, 'System Reliability':{}}
+    dict_max = {'MTTR':0, 'MTBF':0, 'System Reliability':0}
     dict_max_num = {'MTTR':0, 'MTBF':0, 'System Reliability':0}
     for i in range(len(b)):
         if b[i].name.lower() in names_mtbf:
@@ -180,35 +177,35 @@ def finding_num(b):
             num = num * 8760
             b[i].num = str(int(round(num))) + str(' ') + str('hours')
         elif '%' in b[i].num:
-            b[i].num = b[i].num.replace('%','')
+            b[i].num = b[i].num.replace('%', '')
             b[i].num = float(b[i].num)/100
             b[i].num = float(str(b[i].num)[:6])
         elif b[i].name == 'System Reliability':
             try:
-                b[i].num = (b[i].num).replace(' ','')
-                b[i].num = (b[i].num).replace(',','.')
+                b[i].num = (b[i].num).replace(' ', '')
+                b[i].num = (b[i].num).replace(',', '.')
                 b[i].num = float(b[i].num)
                 b[i].num = float(str(b[i].num)[:6])
             except:
-                b[i].num = float((b[i].num).replace(b[i].num[len(b[i].num)-1],''))
+                b[i].num = float((b[i].num).replace(b[i].num[len(b[i].num)-1], ''))
                 b[i].num = float(str(b[i].num)[:6])
         else:
-            b[i].num = str(int(float(((b[i].num).replace(',','')).split(' ')[0]))) + str(' ') + str('hours')
+            b[i].num = str(int(float(((b[i].num).replace(',', '')
+                                     ).split(' ')[0]))) + str(' ') + str('hours')
         try:
             num = int((b[i].num).split(' ')[0])
         except:
             num = b[i].num
-        print(b[i].name,b[i].num)
+        print(b[i].name, b[i].num)
         try:
             dict_num[b[i].name][num] += 1
         except:
             dict_num[b[i].name][num] = 1
     print(dict_num)
-    
     #Matching value is the most repeatable one.
     for name in dict_num:
         for num in dict_num[name]:
-                if dict_num[name][num] > dict_max_num[name]:
-                    dict_max_num[name] = dict_num[name][num]
-                    dict_max[name] = num
+            if dict_num[name][num] > dict_max_num[name]:
+                dict_max_num[name] = dict_num[name][num]
+                dict_max[name] = num
     return dict_max
